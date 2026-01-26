9a0b01db9125f5c85988b863d68a9eba529555f7
Matheus Rodrigues <matheusuniversidade@hotmail.com>
2026-01-25 23:24:52 -0300

    Added background animation

--- frontend/src/pages/Config/Config.jsx
+++ frontend/src/pages/Config/Config.jsx
@@ -15,26 +15,28 @@ export default function Config() {
 
             <BackToMenu />
 
-            <div className="main-settings">
-                <div className="setting-group">
-                    <h3>Música de Fundo</h3>
-                    <input type="checkbox" checked={hasMusic} onChange={(e) => setHasMusic(e.target.checked)} />
-                    <input type="range" disabled={!hasMusic} value={musicVolume} onChange={(e) => setMusicVolume(Number(e.target.value))} />
+            <div className="settings">
+                <div className="main-settings">
+                    <div className="setting-group">
+                        <h3>Música de Fundo</h3>
+                        <input type="checkbox" checked={hasMusic} onChange={(e) => setHasMusic(e.target.checked)} />
+                        <input type="range" disabled={!hasMusic} value={musicVolume} onChange={(e) => setMusicVolume(Number(e.target.value))} />
+                    </div>
+                    <div className="setting-group">
+                        <h3>Efeitos Sonoros</h3>
+                        <input type="checkbox" checked={hasSfx} onChange={(e) => setHasSfx(e.target.checked)} />
+                        <input type="range" disabled={!hasSfx} value={sfxVolume} onChange={(e) => setSfxVolume(e.target.value)} />
+                    </div>
+                    <div className="setting-group">
+                        <h3>Animações de Fundo</h3>
+                        <input type="checkbox" checked={hasAnimBg} onChange={(e) => setHasAnimBg(e.target.checked)} />
+                    </div>
                 </div>
-                <div className="setting-group">
-                    <h3>Efeitos Sonoros</h3>
-                    <input type="checkbox" checked={hasSfx} onChange={(e) => setHasSfx(e.target.checked)} />
-                    <input type="range" disabled={!hasSfx} value={sfxVolume} onChange={(e) => setSfxVolume(e.target.value)} />
-                </div>
-                <div className="setting-group">
-                    <h3>Animações de Fundo</h3>
-                    <input type="checkbox" checked={hasAnimBg} onChange={(e) => setHasAnimBg(e.target.checked)} />
-                </div>
-            </div>
 
-            <div className="user-settings">
-                <button className="logout-button">Sair da Conta</button>
-                <button className="delete-account-button">Excluir Conta</button>
+                <div className="user-settings">
+                    <button className="logout-button">Sair da Conta</button>
+                    <button className="delete-account-button">Excluir Conta</button>
+                </div>
             </div>
         </div>
     )
