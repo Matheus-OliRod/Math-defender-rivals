7342ab93cd164702c56efe7630cd362a6e66b357
Matheus Rodrigues <matheusuniversidade@hotmail.com>
2026-01-29 19:30:42 -0300

    Enhancing Compedium

--- frontend/src/pages/HowToPlay/HowToPlay.jsx
+++ frontend/src/pages/HowToPlay/HowToPlay.jsx
@@ -17,8 +17,8 @@ export default function HowToPlay() {
 
                 <h1>Dificuldades</h1>
                 <p>
-                    Os níveis de dificuldade aumentam conforme o tempo e a quantidade de questões resolvidas. Na primeira partida a dificuldade parte de operações básicas de adição e subtração, subindo para multiplicação, divisão, expondenciação e diversos outros operadores, até subir ao nível das equações de nível Oficial. <br /> <br />
-                    Por motivos de nivelamento e oportunidade, há também um guia matemático para quando um chegar a um nível novo, liberando informações sobre como é resolvido uma equação do nível, habilitando para que você, soldado, possa entender e aprender sobre o próximo nível de desafio e permita escalar ainda mais os ranks. <br /> <br />
+                    Os níveis de dificuldade aumentam conforme a quantidade de questões resolvidas. Na primeira partida a dificuldade parte de operações básicas de adição e subtração, subindo para multiplicação, divisão, expondenciação.... <br /> <br />
+                    Por motivos de nivelamento e oportunidade, há também um guia matemático (Compêndio) para permitir que mesmo sem conhecimento prévio de como se entende e resolve um problema matemático, você, soldado, possa aprender para prosseguir e superar os seus adversários e rivais. <br /> <br />
 
                     O nível em que se começa a partida depende da partida anterior, considerando que a primeira partida sempre irá começar em nível de operações básicas (Adição e subtração). <br /> <br />
                     Exemplificando: Se o soldado chegou até ao nível 7, a próxima partida irá começar a partir do nível 5, onde poderá facilitar o aumento de pontuação sem ter de esperar os desafios maiores por tanto tempo. <br /> <br />
@@ -26,14 +26,13 @@ export default function HowToPlay() {
 
                 <h1>Chefões</h1>
                 <p>
-                    Os chefões são equações de barreira entre os níveis, aparecem a cada 15 questões resolvidas. <br /> <br />
+                    Os chefões são equações de barreira entre os níveis, aparecem a cada 10 questões resolvidas. <br /> <br />
                     Os chefões são o indicador de um novo nível, permitindo que o jogador acesse seu guia instrucional de resolução. <br /> <br />
-                    Ao falhar em derrotar um chefão, o jogador irá perder duas vidas e irá permancer no nível anterior ao chefão.
                 </p>
 
                 <h1>Fim de Jogo</h1>
                 <p>
-                    O Soldado possui três vidas, para cada questão que passa de sua zona de controle é perdido uma vida, duas se for um chefe. <br /> <br />
+                    O Soldado possui três vidas, para cada questão que passa de sua zona de controle é perdido uma vida. <br /> <br />
                     Ao ficar sem vidas, o soldado é mandado de volta para a estação (A tela inicial) para que possa revisar seus conhecimentos e se preparar para a próxima batalha.
                 </p>
             </div>
